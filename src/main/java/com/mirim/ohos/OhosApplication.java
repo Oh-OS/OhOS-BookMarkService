801e78b2386232b64cff8ec4861cedbb774715d5
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-25 21:32:58 +0900

    init

---
+++ src/main/java/com/mirim/ohos/OhosApplication.java
@@ -0,0 +1,13 @@
+package com.mirim.ohos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mirim/ohos/OhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirim.ohos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
