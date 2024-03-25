d4c47e9b1f5098e0e5067b1b0d879e73a1e55f23
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-25 21:49:47 +0900

    feat: BookMarkController 추가

---
+++ src/main/java/com/mirim/ohos/controller/BookMarkController.java
@@ -0,0 +1,36 @@
+package com.mirim.ohos.controller;
+import com.mirim.ohos.dto.bookmark.AddBookMarkRequestDto;
+import com.mirim.ohos.dto.bookmark.BookMarkResponseDto;
+import com.mirim.ohos.service.BookMarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookmarks")
+@RequiredArgsConstructor
+public class BookMarkController {
+
+    private final BookMarkService bookMarkService;
+
+    @PostMapping
+    public ResponseEntity<BookMarkResponseDto> saveBookMark(@RequestBody AddBookMarkRequestDto requestDto) {
+        BookMarkResponseDto savedBookMark = bookMarkService.saveBookMark(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedBookMark);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookMarkResponseDto>> getAllBookMarks() {
+        List<BookMarkResponseDto> allBookMarks = bookMarkService.getAllBookMarks();
+        return ResponseEntity.ok().body(allBookMarks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookMarkResponseDto> getBookMarkById(@PathVariable Long id) {
+        BookMarkResponseDto bookMark = bookMarkService.getBookMarkById(id);
+        return ResponseEntity.ok().body(bookMark);
+    }
+}
